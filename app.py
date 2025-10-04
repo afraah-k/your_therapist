9756f8cdd6db3b1e811e4d2eb3370a5299312b72
Afrah Khan <kafrah23@gmail.com>
2025-10-04 17:06:51 +0530

    Fixed supabase connection issues

--- app.py
+++ app.py
@@ -332,14 +332,14 @@ elif role == "Therapist":
                         audio_url = f"{SUPABASE_URL}/storage/v1/object/public/therapist-audio/{file_path}"
 
                     if audio_bytes or text_answer:
-                        supabase.table("therapist_answers").upsert(
+                         supabase.table("therapist_answers").upsert(
                         {
                             "therapist_id": st.session_state["therapist_id"],
                             "question_id": q_id,
                             "answer_text": text_answer if text_answer else None,
                             "audio_url": audio_url
                         },
-                        on_conflict=["therapist_id", "question_id"]
+                        on_conflict="unique_therapist_question" # <-- FIXED
                     ).execute()
 
                 st.session_state["therapist_all_submitted"] = True
