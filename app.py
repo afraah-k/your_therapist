87ce66eebf093b83003de145a5abc84bf3f48b64
Afrah Khan <kafrah23@gmail.com>
2025-10-07 11:41:27 +0530

    removed on_conflict

--- app.py
+++ app.py
@@ -341,8 +341,7 @@ elif role == "Therapist":
                             "question_id": q_id,
                             "answer_text": text_answer if text_answer else None,
                             "audio_url": audio_url
-                        },
-                        on_conflict=["therapist_id", "question_id"]
+                        }
                     ).execute()
 
 
